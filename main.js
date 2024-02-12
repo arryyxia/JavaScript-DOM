a1997e5d610dd5ff98c897757fd2420761e5264e
mht.aryn <xxryaan3010@gmail.com>
2024-02-12 14:47:04 +0800

    styling element 2

--- main.js
+++ main.js
@@ -21,3 +21,16 @@
 // const container = document.querySelectorAll('div');
 // console.log(container);
 
+// == styiling an element ==
+// const title = document.querySelector('#main-heading');
+// inline styling *1 0 0 0
+// title.style.color = '#00f';
+
+// const listItems = document.querySelectorAll('.list-item');
+// css property written with camel case
+
+// for ( let i = 0; i < listItems.length; i++){
+//     listItems[i].style.fontSize = '1em';
+// }
+
+console.log();
