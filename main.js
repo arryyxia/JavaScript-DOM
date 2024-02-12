40bb29e60aa6067fb858453de3e43af56d264cf6
mht.aryn <xxryaan3010@gmail.com>
2024-02-12 11:13:41 +0800

    trying 5 dom selector

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 // DOM Manipulation
 
 // 1. GetElementByID()
+// const title  = document.getElementById('main-heading');
+// console.log(title);
 
 // 2. GetElementByClassName()
+// const listItems = document.getElementsByClassName('list-item');
+// console.log(listItems)
+
 
 // 3. getElementByTagName()
+// const listTag = document.getElementsByTagName('li');
+// console.log(listTag)
 
 // 4. querySelector()
+// const container = document.querySelector('div');
+// console.log(container);
 
 // 5. quearySelectorAll()
+const container = document.querySelectorAll('div');
+console.log(container);
