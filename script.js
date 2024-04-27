5d7c792046d1fd2d9ac3f3e29aca0f6be8484594
Mahatma Arrayyan <xxryaan3010@gmail.com>
2024-04-27 10:39:08 +0800

    querySelector

--- script.js
+++ script.js
@@ -14,5 +14,14 @@ for (let i = 0; i <= 3; i++){
     paragraph[i].style.backgroundColor = 'gray';
 }
 
+// getElementsByClassName()
+const p1 = document.getElementsByClassName('p1')[0];
 
-console.log(paragraph)
+p1.innerHTML = 'berubah lewat javascript'
+
+// document.querySelector()
+const listKeren = document.querySelector('#b ul li:nth-child(2)');
+
+listKeren.style.backgroundColor = 'blue';
+
+// console.log(paragraph)
