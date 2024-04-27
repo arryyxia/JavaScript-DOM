e03b808c4d648a1236ccbb2110cf7b5649325929
Mahatma Arrayyan <xxryaan3010@gmail.com>
2024-04-27 10:59:00 +0800

    changing root node

--- script.js
+++ script.js
@@ -1,27 +1,99 @@
-// DOM selection
+// // DOM selection
 
-// getElementById()
-const title = document.getElementById('judul');
+// // getElementById()
+// const title = document.getElementById('judul');
 
-title.style.color = 'white';
-title.style.backgroundColor = 'grey';
-title.innerHTML = 'Brody';
+// title.style.color = 'white';
+// title.style.backgroundColor = 'grey';
+// title.innerHTML = 'Brody';
 
-// getElementsByTagName()
-const paragraph = document.getElementsByTagName('p');
+// // getElementsByTagName()
+// const paragraph = document.getElementsByTagName('p');
 
-for (let i = 0; i <= 3; i++){
-    paragraph[i].style.backgroundColor = 'gray';
-}
+// for (let i = 0; i <= 3; i++){
+//     paragraph[i].style.backgroundColor = 'gray';
+// }
 
-// getElementsByClassName()
-const p1 = document.getElementsByClassName('p1')[0];
+// // getElementsByClassName()
+// const p1 = document.getElementsByClassName('p1')[0];
 
-p1.innerHTML = 'berubah lewat javascript'
+// p1.innerHTML = 'berubah lewat javascript'
 
-// document.querySelector()
-const listKeren = document.querySelector('#b ul li:nth-child(2)');
+// // document.querySelector()
+// const listKeren = document.querySelector('#b ul li:nth-child(2)');
 
-listKeren.style.backgroundColor = 'blue';
+// listKeren.style.backgroundColor = 'blue';
 
-// console.log(paragraph)
+// // document.querySelectorAll()
+// const semuaP = document.querySelectorAll('p');
+
+// for (let i = 0; i <= 3; i++){
+//     semuaP[i].style.backgroundColor = 'brown';
+// }
+// // console.log(paragraph)
+
+
+
+
+
+
+
+
+
+const b = document.getElementById('b');
+const p4 = b.getElementsByTagName('p')[0];
+
+p4.innerHTML = 'bimsalabim berubah';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
